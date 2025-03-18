13ed75b8f9e2a23bb46ca544b6d7beba3807f9f5
Vinit Mittal <vinitmittal14@gmail.com>
2025-03-18 12:55:31 +0530

    fixed the issue related to react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,25 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import HomePage from "./pages/HomePage.jsx";
+import About from "./pages/About.jsx";
+import "./index.css";
+import NotFound from "./pages/NotFound.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <NotFound />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
