9552634bed69039a6f67f82f992750942d72c969
Vinit Mittal <vinitmittal14@gmail.com>
2025-06-22 22:48:12 +0530

    fixed scroll issue

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,12 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+import ScrollToTop from "../ScrollToTop";
 
 const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen bg-gray-100">
+      <ScrollToTop/>
       <Header />
       <main className="flex-grow"> {/* Adjusted padding */}
         <Outlet />
